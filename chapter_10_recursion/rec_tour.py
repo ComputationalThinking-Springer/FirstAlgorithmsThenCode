d1f37f5deab6b4195ff9eddf3ae3714236e31274
Computational Thinking: First Algorithms, then Code <36237002+ComputationalThinking-Springer@users.noreply.github.com>
2018-02-08 20:52:41 +0100

    Update and rename chapter_10_the_recursivity/rec_tournament.py to chapter_10_recursion/rec_tour.py

--- chapter_10_recursion/rec_tour.py
+++ chapter_10_recursion/rec_tour.py
@@ -1,33 +1,33 @@
-# Program REC_TOURNAMENT in Python
-# Figure 10.1 from the book "Il Pensiero Computazionale: dagli algoritmi al coding"
+# Program REC_TOUR in Python
+# Figure 10.1 from the book "Computational Thinking: First Algorithms, Then Code"
 # Authors: Paolo Ferragina and Fabrizio Luccio
-# Published by Il Mulino
+# Published by Springer
 
 
 # loads mathematical functions
 import math
 
 
-def rec_tournament(set, i, j):
+def rec_tour(set, i, j):
     """
     Search the winner in a table using the recursion. 
-    The participants are contained in a vector 'set' of n = 2^k elements, 
+    The participants are stored in a vector 'set' of n = 2^k elements, 
     but the program refers to a generic sub-tournament among the participants 
     contained in the portion set[i:j], inclusive.
-    :param set: set of integers where to search the maximum on
+    :param set: set of integers where to search for the maximum
     :param i: lower index of set
     :param j: upper index of set
     """
 
-    if j == i + 1:                          # extreme situation with two player
+    if j == i + 1:                          # extreme situation with only two player
         if set[i] > set[j]:
             return set[i]
         else:
             return set[j]
 
     m = int(math.floor((i + j) / 2))
-    v1 = rec_tournament(set, i, m)          # v1 is the winner of the first sub-tournament
-    v2 = rec_tournament(set, m + 1, j)      # v2 is the winner of the second sub-tournament
+    v1 = rec_tourn(set, i, m)          # v1 is the winner of the first sub-tournament
+    v2 = rec_tourn(set, m + 1, j)      # v2 is the winner of the second sub-tournament
 
     if v1 > v2:
         return v1
@@ -39,7 +39,7 @@ def main():
 
     set = [24, 15, 7, 9, 20, 49, 33, 35, 22, 40, 52, 12, 62, 30, 8, 43]
 
-    print "the maximum is %d" % rec_tournament(set, 0, len(set) - 1)
+    print "the maximum is %d" % rec_tour(set, 0, len(set) - 1)
 
 
 if __name__ == "__main__":
