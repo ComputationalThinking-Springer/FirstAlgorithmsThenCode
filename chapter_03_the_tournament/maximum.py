32fa24947943f044803f098e6d88802c0deaf355
Computational Thinking: First Algorithms, then Code <36237002+ComputationalThinking-Springer@users.noreply.github.com>
2018-02-08 19:48:46 +0100

    Update maximum.py

--- chapter_03_the_tournament/maximum.py
+++ chapter_03_the_tournament/maximum.py
@@ -1,7 +1,7 @@
 # Program MAXIMUM in Python
-# Figure 3.2 from the book "Il Pensiero Computazionale: dagli algoritmi al coding"
+# Figure 3.2 from the book "Computational Thinking: First Algorithms, Then Code"
 # Authors: Paolo Ferragina and Fabrizio Luccio
-# Published by Il Mulino
+# Published by Springer
 
 
 def maximum(set):
