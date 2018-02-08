c3f969d4b5d89f2784ca9f5de583189adb4bf1bf
Computational Thinking: First Algorithms, then Code <36237002+ComputationalThinking-Springer@users.noreply.github.com>
2018-02-08 19:53:07 +0100

    Update binary_dispositions.py

--- chapter_05_secret_messages/binary_dispositions.py
+++ chapter_05_secret_messages/binary_dispositions.py
@@ -1,7 +1,7 @@
 # Program BINARY_DISPOSITIONS in Python
-# Figure 6.6 from the book "Il Pensiero Computazionale: dagli algoritmi al coding"
+# Figure 5.6 from the book "Computational Thinking: First Algorithms, Then Code"
 # Authors: Paolo Ferragina and Fabrizio Luccio
-# Published by Il Mulino
+# Published by Springer
 
 
 def binary_dispositions(n):
