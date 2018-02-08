be50bd15628206ca11fd5b092a3e11be7fa9384a
Computational Thinking: First Algorithms, then Code <36237002+ComputationalThinking-Springer@users.noreply.github.com>
2018-02-08 20:02:31 +0100

    Update degrees.py

--- chapter_07_easy_and_difficult_problems/degrees.py
+++ chapter_07_easy_and_difficult_problems/degrees.py
@@ -1,7 +1,7 @@
 # Program DEGREES in Python
-# Figure 7.5 from the book "Il Pensiero Computazionale: dagli algoritmi al coding"
+# Figure 7.5 from the book "Computational Thinking: First Algorithms, Then Code"
 # Authors: Paolo Ferragina and Fabrizio Luccio
-# Published by Il Mulino
+# Published by Springer
 
 
 # loads mathematical functions
