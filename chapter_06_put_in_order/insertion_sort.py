ee74d29dc3c7184f50061a02d269cc393b40cbe6
Computational Thinking: First Algorithms, then Code <36237002+ComputationalThinking-Springer@users.noreply.github.com>
2018-02-08 19:58:11 +0100

    Update insertion_sort.py

--- chapter_06_put_in_order/insertion_sort.py
+++ chapter_06_put_in_order/insertion_sort.py
@@ -1,7 +1,7 @@
 # Program INSERTION_SORT in Python
-# Figure 5.2 from the book "Il Pensiero Computazionale: dagli algoritmi al coding"
+# Figure 6.2 from the book "Computational Thinking: First Algorithms, Then Code"
 # Authors: Paolo Ferragina and Fabrizio Luccio
-# Published by Il Mulino
+# Published by Springer
 
 
 def insertion_sort(c):
