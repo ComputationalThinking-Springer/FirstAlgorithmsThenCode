ccd1574e8ad5579b7ce258c9a43c830a3b801c19
Computational Thinking: First Algorithms, then Code <36237002+ComputationalThinking-Springer@users.noreply.github.com>
2018-02-08 20:16:33 +0100

    Update tournament.py

--- chapter_03_the_tournament/tournament.py
+++ chapter_03_the_tournament/tournament.py
@@ -11,14 +11,14 @@ import math
 def tournament(set):
     """
     Search the maximum in a table with n = 2^k partecipants.
-    :param set: set of integers on which to seek the maximum
+    :param set: set of integers in which to search for the maximum
     """
 
     n = len(set)                        # n is the number of elements of set
-    k = int(math.log(n, 2))             # leaves level
+    k = int(math.log(n, 2))             # level of the leaves 
 
     p = list()
-    for i in range(0, n):               # copy set in p
+    for i in range(0, n):               # copy the set in p
         p.append(set[i])
 
     h = k                               # h is the current level
@@ -26,19 +26,19 @@ def tournament(set):
         i = 0
         v = list()
                                         # x**y is the python expression for x^y
-        while i <= 2**h - 2:            # carica in v i vincitori
-            if p[i] > p[i + 1]:         # p[i] e' il vincitore
+        while i <= 2**h - 2:            # load in v the winners
+            if p[i] > p[i + 1]:         # p[i] is the winner
                 v.append(p[i])
-            else:                       # p[i+1] e' il vincitore
+            else:                       # p[i+1] is the winner
                 v.append(p[i + 1])
             i = i + 2
 
-        for i in range(0, 2**(h-1)):    # trasferisce in p i vincitori
+        for i in range(0, 2**(h-1)):    # copies in p the winners
             p[i] = v[i]
 
-        h = h - 1                       # si sposta al livello superiore
+        h = h - 1                       # moves to the level above
 
-    print "il massimo e' %d" % p[0]
+    print "the maximum is %d" % p[0]
 
 
 def main():
