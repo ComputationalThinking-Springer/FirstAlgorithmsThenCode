bcd986d86ced97e06994351e53b9c502b2b8b54b
Computational Thinking: First Algorithms, then Code <36237002+ComputationalThinking-Springer@users.noreply.github.com>
2018-02-08 19:50:59 +0100

    Update linear.py

--- chapter_04_a_financial_problem/linear.py
+++ chapter_04_a_financial_problem/linear.py
@@ -1,7 +1,7 @@
 # Program LINEAR in Python
-# Figure 4.5 from the book "Il Pensiero Computazionale: dagli algoritmi al coding"
+# Figure 4.5 from the book "Computational Thinking: First Algorithms, Then Code"
 # Authors: Paolo Ferragina and Fabrizio Luccio
-# Published by Il Mulino
+# Published by Springer
 
 
 def linear(d):
