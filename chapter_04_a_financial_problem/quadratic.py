7fd91a3a01bd60023e12fe66798d760270346e73
Computational Thinking: First Algorithms, then Code <36237002+ComputationalThinking-Springer@users.noreply.github.com>
2018-02-08 19:50:37 +0100

    Update quadratic.py

--- chapter_04_a_financial_problem/quadratic.py
+++ chapter_04_a_financial_problem/quadratic.py
@@ -1,7 +1,7 @@
 # Program QUADRATIC in Python
-# Figure 4.3 from the book "Il Pensiero Computazionale: dagli algoritmi al coding"
+# Figure 4.3 from the book "Computational Thinking: First Algorithms, Then Code"
 # Authors: Paolo Ferragina and Fabrizio Luccio
-# Published by Il Mulino
+# Published by Springer
 
 
 def quadratic(d):
