73e6fd0591ad10e9c2075c2f5b2c8fd7204aa92c
Computational Thinking: First Algorithms, then Code <36237002+ComputationalThinking-Springer@users.noreply.github.com>
2018-02-08 19:48:08 +0100

    Update search2.py

--- chapter_02_algorithms_and_coding/search2.py
+++ chapter_02_algorithms_and_coding/search2.py
@@ -1,7 +1,7 @@
 # Program SEARCH2 in Python
-# Figure 2.4 from the book "Il Pensiero Computazionale: dagli algoritmi al coding"
+# Figure 2.4 from the book "Computational Thinking: First Algorithms, Then Code"
 # Authors: Paolo Ferragina and Fabrizio Luccio
-# Published by Il Mulino
+# Published by Springer
 
 
 def search2(set, info, data):
