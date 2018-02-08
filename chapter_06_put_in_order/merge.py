d08decebf1db203f01fe24dea311b7486f483813
Computational Thinking: First Algorithms, then Code <36237002+ComputationalThinking-Springer@users.noreply.github.com>
2018-02-08 19:58:55 +0100

    Update and rename fusione.py to merge.py

--- chapter_06_put_in_order/merge.py
+++ chapter_06_put_in_order/merge.py
@@ -1,7 +1,7 @@
-# Program FUSION in Python
-# Figure 5.4 from the book "Il Pensiero Computazionale: dagli algoritmi al coding"
+# Program MERGE in Python
+# Figure 6.4 from the book "Computational Thinking: First Algorithms, Then Code"
 # Authors: Paolo Ferragina and Fabrizio Luccio
-# Published by Il Mulino
+# Published by Springer
 
 
 def fusion(a, b):
