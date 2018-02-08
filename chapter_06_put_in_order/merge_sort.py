2b9d996aab4971f413802eef777539eb559863f8
Computational Thinking: First Algorithms, then Code <36237002+ComputationalThinking-Springer@users.noreply.github.com>
2018-02-08 19:59:29 +0100

    Update merge_sort.py

--- chapter_06_put_in_order/merge_sort.py
+++ chapter_06_put_in_order/merge_sort.py
@@ -1,7 +1,7 @@
 # Program MERGE_SORT in Python
-# Figure 5.6 from the book "Il Pensiero Computazionale: dagli algoritmi al coding"
+# Figure 6.6 from the book "Computational Thinking: First Algorithms, Then Code"
 # Authors: Paolo Ferragina and Fabrizio Luccio
-# Published by Il Mulino
+# Published by Springer
 
 
 # loads mathematical functions
